c65dcbd4dee0cbab4e522a8f94e6b1373630fd06
saifmehsud <1saifmehsud@gmail.com>
2026-01-23 04:49:24 -0800

    remove testing route

--- src/api/app.js
+++ src/api/app.js
@@ -54,23 +54,7 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/upload-local', upload.single('avatar'), (req, res) => {
 
-    console.log(`File uploaded: ${req.file}`);
-
-    if (!req.file) {
-        return res.status(400).json({
-            success: false,
-            message: 'File not uploaded'
-        });
-    }
-    return res.status(200).json({
-        success: true,
-        message: 'File uploaded successfully',
-        data: req.file
-    });
-
-})
 
 app.use('*', (req, res) => {
     res.status(404).json({
